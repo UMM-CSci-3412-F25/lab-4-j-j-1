36b1bd4ca1dcd8675c4291ff6a195c602fa43e70
James Swearingen <swear051@morris.umn.edu>
2025-09-30 11:16:52 -0500

    it doesn't work
    weird jumble of characters and I have to git restore the files it reads from because it keeps breaking them

---
+++ file_disemvowel/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "file_disemvowel.h"
+
+int main(int argc, char *argv[]) {
+  char *line;
+  size_t size;
+  FILE *inputFile = stdin;
+  FILE *outputFile = stdout;
+  size = 100;
+  line = (char*) malloc (size + 1);
+  if (argc >= 1) {
+    printf("Arg 1: %s\n", argv[0]);
+    inputFile = fopen(argv[0], "r");
+  }
+  if (argc >= 2) {
+    printf("Arg 2: %s\n", argv[1]);
+    outputFile = fopen(argv[1], "w");
+  }
+
+  while (getline(&line, &size, inputFile) > 0) {
+    char *result = disemvowel(line);
+    fprintf(outputFile, "%s\n", result);
+    // printf("%s\n", result);
+    free(result);
+  }
+  
+  free(line);
+  fclose(inputFile);
+  fclose(outputFile);
+}
